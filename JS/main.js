b79a45b568d1cb3c2d8f28ab6ae32db50609cff1
Filip Pędracki <filip.pedracki@gmail.com>
2020-05-16 21:52:23 +0200

    practise js

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,12 @@
-const wiek = 31;
-const nazwa = 'Filip Pędracki';
+const age = 31;
+const name = 'Filip';
 
-console.log(`Siema nazywam się ${nazwa} i mam ${wiek}  lata`)
+console.log(`Siema nazywam się ${name} i mam ${age} lata`);
+
+
+
+function greet(age, name){
+    console.log(`Cześć internauto, mam na imię ${name} i mam ${age} lat(a)`);
+      
+}
+greet(age, name);
