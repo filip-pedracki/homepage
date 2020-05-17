3354697f6daa5d4611ef2b83459c7d0c885cc5e8
Filip Pędracki <filip.pedracki@gmail.com>
2020-05-17 11:27:47 +0200

    practice JS again

--- JS/main.js
+++ JS/main.js
@@ -9,4 +9,22 @@ function greet(age, name){
     console.log(`Cześć internauto, mam na imię ${name} i mam ${age} lat(a)`);
       
 }
-greet(age, name);
+
+greet(age, name)
+
+
+function createContent(querySelectorContent, content){
+const element = document.querySelector(querySelectorContent);
+element.innerHTML = content;
+}
+
+createContent('.section__about-me--js', 'Coś o mnie')
+
+
+function calculateMultiplication(number, number1){
+    const multiplication = number * number1;
+    calculate = multiplication;
+    console.log(multiplication);
+}
+
+calculateMultiplication(3,2);
